81b40ed23a56e536d64f5ddacb4e3d94a937a781
Daniel Sundermeier <daniel@hof-sundermeier.de>
2019-11-17 06:20:04 +0100

    deployment

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return redirect(route('login'));
 });
 
+Route::post('deploy', 'DeploymentController@store');
+
 Auth::routes();
 
 Route::middleware(['auth'])->group(function () {
