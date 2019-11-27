ad77c893f5c67c32ea8f3df55b48a969e50b203a
Daniel Sundermeier <daniel@hof-sundermeier.de>
2019-11-27 10:40:19 +0100

    standalone links

--- public/js/app.js
+++ public/js/app.js
@@ -72795,6 +72795,37 @@ Highcharts.setOptions({
   }
 });
 
+(function (document, navigator, standalone) {
+  // prevents links from apps from oppening in mobile safari
+  // this javascript must be the first script in your <head>
+  if (standalone in navigator && navigator[standalone]) {
+    $('#history-back').removeClass('hidden');
+    var curnode,
+        location = document.location,
+        stop = /^(a|html)$/i;
+    document.addEventListener('click', function (e) {
+      curnode = e.target;
+
+      while (!stop.test(curnode.nodeName)) {
+        curnode = curnode.parentNode;
+      } // Condidions to do this only on links to your own app
+      // if you want all links, use if('href' in curnode) instead.
+
+
+      if ('href' in curnode && // is a link
+      (chref = curnode.href).replace(location.href, '').indexOf('#') && ( // is not an anchor
+      !/^[a-z\+\.\-]+:/i.test(chref) || // either does not have a proper scheme (relative links)
+      chref.indexOf(location.protocol + '//' + location.host) === 0) // or is in the same protocol and domain
+      ) {
+          e.preventDefault(); //$('#loading-wrapper').fadeIn(400);
+
+          location.href = curnode.href;
+        }
+    }, false);
+  }
+})(document, window.navigator, 'standalone');
+
+
 Vue.use(_plugins_flash_js__WEBPACK_IMPORTED_MODULE_0__["default"]);
 /**
  * The following block of code may be used to automatically register your
