f97f6890d5ac7304f6e7d43c8e038b626b726068
Daniel Sundermeier <daniel@hof-sundermeier.de>
2019-12-02 06:13:59 +0100

    pwa links

--- public/js/app.js
+++ public/js/app.js
@@ -73478,6 +73478,37 @@ Vue.component('user-balance-table', __webpack_require__(/*! ./components/user/ba
 var app = new Vue({
   el: '#app'
 });
+
+(function (document, navigator, standalone) {
+  // prevents links from apps from oppening in mobile safari
+  // this javascript must be the first script in your <head>
+  if (standalone in navigator && navigator[standalone]) {
+    $('#history-back').removeClass('hidden');
+    var curnode,
+        location = document.location,
+        stop = /^(a|html)$/i;
+    document.addEventListener('click', function (e) {
+      curnode = e.target;
+
+      while (!stop.test(curnode.nodeName)) {
+        curnode = curnode.parentNode;
+      } // Condidions to do this only on links to your own app
+      // if you want all links, use if('href' in curnode) instead.
+
+
+      if ('href' in curnode && // is a link
+      (chref = curnode.href).replace(location.href, '').indexOf('#') && ( // is not an anchor
+      !/^[a-z\+\.\-]+:/i.test(chref) || // either does not have a proper scheme (relative links)
+      chref.indexOf(location.protocol + '//' + location.host) === 0) // or is in the same protocol and domain
+      ) {
+          e.preventDefault(); //$('#loading-wrapper').fadeIn(400);
+
+          location.href = curnode.href;
+        }
+    }, false);
+  }
+})(document, window.navigator, 'standalone');
+
 $('#menu-toggle').click(function () {
   $('#nav, #content-container').toggleClass('active');
   $('.collapse.in').toggleClass('in');
