35ea75645097062738affbf10f06a7df297df344
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-01-14 03:42:22 +0100

    remove balance

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -42,7 +42,6 @@
                             </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
                                 <a class="dropdown-item" href="/user/settings">Einstellungen</a>
-                                <a class="dropdown-item" href="/user/balance">Guthaben</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                    onclick="event.preventDefault();
                                                  document.getElementById('logout-form').submit();">
