051d46d65860bb4ad024ce99c96a862f664132ba
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-03-20 04:16:17 +0100

    JS fix

--- resources/js/app.js
+++ resources/js/app.js
@@ -96,51 +96,52 @@ common.loadLanguage(window.Laravel.locale, true).then((i18n) => {
         el: '#app',
     });
 
+    (function(document,navigator,standalone) {
+        // prevents links from apps from oppening in mobile safari
+        // this javascript must be the first script in your <head>
+        if ((standalone in navigator) && navigator[standalone]) {
+            $('#history-back').removeClass('hidden');
+            var curnode, location=document.location, stop=/^(a|html)$/i;
+            document.addEventListener('click', function(e) {
+                curnode=e.target;
+                while (!(stop).test(curnode.nodeName)) {
+                    curnode=curnode.parentNode;
+                }
+                // Condidions to do this only on links to your own app
+                // if you want all links, use if('href' in curnode) instead.
+                if(
+                    'href' in curnode && // is a link
+                    (chref=curnode.href).replace(location.href,'').indexOf('#') && // is not an anchor
+                    (   !(/^[a-z\+\.\-]+:/i).test(chref) ||                       // either does not have a proper scheme (relative links)
+                        chref.indexOf(location.protocol+'//'+location.host)===0 ) // or is in the same protocol and domain
+                ) {
+                    e.preventDefault();
+                    //$('#loading-wrapper').fadeIn(400);
+                    location.href = curnode.href;
+                }
+            }, false);
+        }
+    })(document,window.navigator,'standalone');
+
+    $('#menu-toggle').click(function() {
+        $('#nav, #content-container').toggleClass('active');
+        $('.collapse.in').toggleClass('in');
+        $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+    });
+
+    $('.collapse', 'nav#nav').on('show.bs.collapse', function(){
+        $('a[data-target="#' + $(this).attr('id') +'"] i.fas', 'nav#nav').toggleClass("fa-caret-right fa-caret-down");
+    }).on('hide.bs.collapse', function(){
+        $('a[data-target="#' + $(this).attr('id') +'"] i.fas', 'nav#nav').toggleClass("fa-caret-down fa-caret-right");
+    });
+
+    $('#message-create').on('show.bs.modal', function (e) {
+        axios.get('/order/' + $(e.relatedTarget).attr('data-model-id') + '/message/create')
+            .then(function (response) {
+                $('#message-text', '#message-create').val(response.data.body);
+        });
+    });
+
     return app;
 });
 
-(function(document,navigator,standalone) {
-    // prevents links from apps from oppening in mobile safari
-    // this javascript must be the first script in your <head>
-    if ((standalone in navigator) && navigator[standalone]) {
-        $('#history-back').removeClass('hidden');
-        var curnode, location=document.location, stop=/^(a|html)$/i;
-        document.addEventListener('click', function(e) {
-            curnode=e.target;
-            while (!(stop).test(curnode.nodeName)) {
-                curnode=curnode.parentNode;
-            }
-            // Condidions to do this only on links to your own app
-            // if you want all links, use if('href' in curnode) instead.
-            if(
-                'href' in curnode && // is a link
-                (chref=curnode.href).replace(location.href,'').indexOf('#') && // is not an anchor
-                (   !(/^[a-z\+\.\-]+:/i).test(chref) ||                       // either does not have a proper scheme (relative links)
-                    chref.indexOf(location.protocol+'//'+location.host)===0 ) // or is in the same protocol and domain
-            ) {
-                e.preventDefault();
-                //$('#loading-wrapper').fadeIn(400);
-                location.href = curnode.href;
-            }
-        }, false);
-    }
-})(document,window.navigator,'standalone');
-
-$('#menu-toggle').click(function() {
-    $('#nav, #content-container').toggleClass('active');
-    $('.collapse.in').toggleClass('in');
-    $('a[aria-expanded=true]').attr('aria-expanded', 'false');
-});
-
-$('.collapse', 'nav#nav').on('show.bs.collapse', function(){
-    $('a[data-target="#' + $(this).attr('id') +'"] i.fas', 'nav#nav').toggleClass("fa-caret-right fa-caret-down");
-}).on('hide.bs.collapse', function(){
-    $('a[data-target="#' + $(this).attr('id') +'"] i.fas', 'nav#nav').toggleClass("fa-caret-down fa-caret-right");
-});
-
-$('#message-create').on('show.bs.modal', function (e) {
-    axios.get('/order/' + $(e.relatedTarget).attr('data-model-id') + '/message/create')
-        .then(function (response) {
-            $('#message-text', '#message-create').val(response.data.body);
-    });
-});
