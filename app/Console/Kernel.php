53051606e4c39a6c2e6304029e9735d9dc08821b
Daniel Sundermeier <daniel@hof-sundermeier.de>
2020-08-31 08:53:31 +0200

    Update Kernel.php

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,9 +27,6 @@ class Kernel extends ConsoleKernel
         // $schedule->command('card:price:sync')->cron('0 */2 * * *');
         // $schedule->command('user:balance:import')->daily();
         $schedule->command('card:price:sync')->dailyAt('03:17');
-        $schedule->command('games:import', [
-            'game' => 0,
-        ])->dailyAt('08:27');
     }
 
     /**
